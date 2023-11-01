365182396cd90f15a8114f654e0ead66bbd0d80e
Marina Varfolomeeva <marina.varfolomeeva@gmail.com>
2023-11-01 11:04:26 +0000

    Refactor code:
    To avoid multiple conversions to int, changed is_valid_integer_in_range() -> bool to get_integer_input_in_range() -> int
    It replaced several repetitive functions for getting user input (boundaries to for random number generation, user guesses)

--- guess_the_number.py
+++ guess_the_number.py
@@ -17,91 +17,116 @@ the message 'Correct, congratulations!'
 
 Change log
 
-- Added counting attempts made by the user. When the number is guessed,
-the program shows the number of attempts (total, valid, invalid).
-- Added the ability to specify the right boundary for random number selection
-(from 1 to n).
-- Added the ability to generate a new number (play again),
+- Add the ability to specify the right boundary 
+for random number selection (from 1 to n).
+- Add the ability to generate a new number (play again),
 after the user has guessed the number.
 
+TODO: 
+- Add counting attempts made by the user. When the number is guessed,
+the program shows the number of attempts (total, valid, invalid).
 """
 
 import random
 
-def is_valid_integer_in_range(value, lower, upper):
-    try:
-        int_value = int(value)
-        return lower <= int_value <= upper
-    except ValueError:
-        return False
+def get_integer_input_in_range(prompt: str, lower: int, upper: int) -> int:
+    """Gets user input as an integer within a given range.
 
-def get_boundaries():
-    print('\nEnter the boundaries of the number range the program will generate')
-    while True:
-        left = input('Left boundary (number from 1 to 100): ')
-        if is_valid_integer_in_range(left, 1, 100):
-            left = int(left)
-            break
-        else:
-            print('Enter an integer from 1 to 100')
+    Args:
+        prompt (str): The prompt to display to the user.
+        lower (int): The lower bound of the range.
+        upper (int): The upper bound of the range.
+    
+    Returns:
+        int: An integer within the given range.
+    """
+    user_input = None
+    while user_input is None or not lower <= user_input <= upper:
+        try:
+            user_input = int(input(prompt + f' from {lower} to {upper}: '))
+            if not lower <= user_input <= upper:
+                print(f"Input must be between {lower} and {upper}")
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+    return user_input
 
-    while True:
-        right = input(f'Right boundary (from {left} to 100): ')
-        if is_valid_integer_in_range(right, left, 100):
-            right = int(right)
-            return left, right
-        else:
-            print(f'Enter an integer from {left} to 100')
+def hint(guess: int, n: int) -> str:
+    """Hints the user if their number is less than, 
+    greater than or equals to the generated one.
 
-def get_user_guess(left, right):
-    count_invalid = 0
-    while True:
-        value = input(f'\nEnter a number from {left} to {right}: ')
-        if is_valid_integer_in_range(value, left, right):
-            return int(value), int(count_invalid)
-        else:
-            count_invalid += 1
-            print(f'Enter an integer from {left} to {right}')
+    Args:
+        guess (integer): The user guess.
+        n (_type_): The generated number.
 
-def play_game(left, right):
+    Returns:
+        str: A hint indicating if the user's number is less than, 
+    greater than or equals to the generated one.
+    """
+    if guess < n:
+        return f'{guess} is less than my number'
+    elif guess > n:
+        return f'{guess} is greater than my number'
+    else:
+        return f'{guess} equals my number'
+
+def play_game():
+    """This function allows a player to guess a randomly generated number 
+    within a specified range.
+
+    The function prompts the user to enter the boundaries of the number 
+    range and generates a random number within that range.It then repeatedly 
+    prompts the user to guess the generated number and provides hints until 
+    the user correctly guesses the number.
+
+    This function doesn't accept any arguments or return values.
+    """
+    print('Enter the range in which you want to generate the number')
+    left = get_integer_input_in_range('Left boundary', 1, 100)
+    right = get_integer_input_in_range('Right boundary', left, 100)
+    print(f'Guess the number from {left} to {right}')
     n = random.randint(left, right)
-    count_valid_attempts = 0
-    count_invalid_attempts = 0
-    print(f'\nGuess the number from {left} to {right}')
-    while True:
-        value, invalid_attempts = get_user_guess(left, right)
-        count_invalid_attempts += invalid_attempts
-        if value == n:
-            count_valid_attempts += 1
-            print('Correct, congratulations!')
-            print(f'\nTotal attempts {count_valid_attempts + count_invalid_attempts}',
-                  f', valid - {count_valid_attempts}, ', 
-                  f'invalid - {count_invalid_attempts}')
-            break
-        elif value < n:
-            print('Your number is less than mine, try again')
-            count_valid_attempts += 1
-        elif value > n:
-            print('Your number is greater than mine, try again')
-            count_valid_attempts += 1
+    guess = None
+    while guess != n:
+        guess = get_integer_input_in_range('Your number', left, right)
+        print(hint(guess, n))
+    print('Congratulations!')
 
-def play_again():
-    while True:
+def wants_to_play_again(prompt: str) -> bool:
+    """Asks the user if they want to play again and 
+    returns a Boolean value.
+
+    Args:
+        prompt (str): A question to ask.
+
+    Returns:
+        bool: True if the user wants to play again, False if not.
+    """
+    valid_responses = {'yes', 'y', 'no', 'n', 'quit', 'q'}
+    response = None
+    print(prompt)
+    while response is None or response not in valid_responses:
+        print('Please enter either "yes", "no", or "quit".')
         response = input('Would you like to play again? (yes, no, quit): ').strip().lower()
-        if response in ['yes', 'y']:
-            return True
-        elif response in ['no', 'n', 'quit', 'q']:
-            return False
-        else:
-            print('Please enter either "yes", "no" or "quit".')
+    return response in {'yes', 'y'}
 
 def main():
+    """Entry point for the Number Guesser game.
+
+    This function initializes the Number Guesser game, displaying a welcome 
+    message and then repeatedly calling the `play_game` function to play the game. 
+    After each game, it asks the player if they want to play again using 
+    the `play_again` function. If the player chooses to play again, 
+    the game continues. If not, the game exits with a farewell message.
+
+    The game loop runs indefinitely until the player decides not to play again.
+
+    This function doesn't accept any arguments or return values.
+    """
     print('\nWelcome to the Number Guesser\n')
     while True:
-        left, right = get_boundaries()
-        play_game(left, right) 
-        if not play_again():
-            print('\nThank you for playing the number guesser. See you again...')
+        play_game()
+        if not wants_to_play_again("Would you like to play again?"):
+            print('\nThank you for playing the number guesser. See you again...\n')
             break
 
 if __name__ == "__main__":
